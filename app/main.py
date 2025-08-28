38f94d0725bdea865f43396795b5cea15580522e
兰政鹏 <63629183+lanzhengpeng@users.noreply.github.com>
2025-08-29 00:25:59 +0800

    改端口

--- app/main.py
+++ app/main.py
@@ -13,4 +13,4 @@ app.include_router(tool.router)
 if __name__ == "__main__":
     import uvicorn
     # 使用导入字符串 "main:app" 代替直接传递 app 对象
-    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
+    uvicorn.run("main:app", host="0.0.0.0", port=8001, reload=True)
